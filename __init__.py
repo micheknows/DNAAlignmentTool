edf14a60c67f3409fc748f556c052404ba31debc
miche <micheknows@gmail.com>
2024-01-14 22:41:50 -0700

    checking

--- __init__.py
+++ __init__.py
@@ -6,10 +6,10 @@ dna_tool = Blueprint('dna_tool', __name__,
                     template_folder="templates",
                     static_folder='static', url_prefix='/dna_tool')
 
-from . import routes
+#from . import routes
 
 
-@dna_tool.route('/test')
-def dna_index2():
-    return "hiya"
+#@dna_tool.route('/test')
+#def dna_index2():
+#    return "hiya"
 
