367eb0866ab8b5653c1d49eb307e7c374e45308e
miche <micheknows@gmail.com>
2024-01-14 19:53:01 -0700

    update submodule reference

--- dna_tool/routes.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for
-from DNAAlignmentTool import dna_tool
-
-@dna_tool.route('/test')
-def test():
-    return "DNA Blueprint Works!"
