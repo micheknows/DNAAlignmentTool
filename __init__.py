ad665148366d1b595d76e989af19993eaa648212
miche <micheknows@gmail.com>
2024-01-14 19:35:02 -0700

    update submodule reference

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ dna_tool = Blueprint('dna_tool', __name__,
                     template_folder="app/templates",
                     static_folder='app/static', url_prefix='/dna-tool')
 
-from .app import routes
+from dna_tool import routes
 
 @dna_tool.route('/test')
 def test():
